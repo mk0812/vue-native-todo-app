d5c8ec0bdb15a1b88b5405c8bacc223938766734
mk_limitbreak <matsuoka812930@gmail.com>
2019-03-21 15:25:53 +0900

    first commit

---
+++ app/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<page>
+		<action-bar title="Todoリスト"></action-bar>
+		<todo></todo>
+	</page>
+</template>
+
+<script>
+import Todo from './Todo.vue'
+export default {
+	components: {
+		Todo
+	}
+}
+</script>
+
+<style scoped>
+    label {
+        text-align: center;
+        font-size: 25;
+    }
+
+    Button {
+        background-color: #00caab;
+    }
+</style>
---
+++ app/components/Todo.vue
@@ -0,0 +1,84 @@
+<template>
+	<grid-layout rows="*,80">
+		<scroll-view row="0">
+			<list-view :items="todos" class="list-group">
+				<template scope="todo">
+					<grid-layout row="1" columns="*, 60">
+					<label :text="todo.title" textWrap="true" :class="{done: todo.done}" @tap="toggleDoneState(todo)"></label>
+					<button col="1" text="削除" @tap="deleteTodo(todo)"></button>
+					</grid-layout>
+				</template>
+			</list-view>
+		</scroll-view>
+		<grid-layout row="1" columns="*, 60" class="footer">
+			<text-field col="0" hint="今日は何をしますか？" v-model="newTodoTitle"></text-field>
+			<button col="1" text="追加" @tap="addTodo()"></button>
+		</grid-layout>
+	</grid-layout>
+</template>
+
+<script>
+import localStorage from 'nativescript-localstorage'
+
+const LOCAL_STORAGE_KEY = 'todos-localstorage'
+
+const todoLocalStorage = {
+	fetch: () => {
+		const todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}')
+		todos.forEach((todo, index) => {
+			todo.id = index
+		})
+		return todos
+	},
+	store: (todos) => {
+		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+	}
+}
+
+export default {
+	data() {
+		return {
+			todos: [],
+			newTodoTitle: '',
+		}
+	},
+	created: function() {
+		this.todos = todoLocalStorage.fetch()
+	},
+	watch: {
+		todos: {
+			handler: function(todos) {
+				todoLocalStorage.store(todos)
+			},
+			deep: true
+		}
+	},
+	methods: {
+		addTodo: function() {
+			if (!this.newTodoTitle) return
+				this.todos.push({
+				id: this.todos.length + 1,
+				title: this.newTodoTitle,
+				done: false
+			})
+			this.newTodoTitle = ''
+		},
+		deleteTodo: function(targetTodo) {
+			this.todos.splice(this.todos.findIndex(function(todo) {
+				return todo.id === targetTodo.id
+			}), 1)
+		},
+		toggleDoneState: function(targetTodo) {
+			const todoIndex = this.todos.findIndex(function(todo) {
+				return todo.id === targetTodo.id
+			})
+
+			this.todos[todoIndex].done = !targetTodo.done
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+
+</style>
